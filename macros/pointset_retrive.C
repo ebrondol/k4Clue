7a2e05162e464638f578ae1ba18dbef0d7a1812b
Erica Brondolin <erica.brondolin@cern.ch>
2022-01-27 14:27:59 +0100

    Document macro

--- macros/pointset_retrive.C
+++ macros/pointset_retrive.C
@@ -8,23 +8,26 @@ void pointset_retrive(TString nEvent = "0")
 {
    std::cout << "Dispaying event#" << nEvent << std::endl;
 
-   TFile* f = new TFile("../build/src/validation_3.root", "read");
+   //Opening validation file
+   TFile* f = new TFile("../build/src/validation.root", "read");
    TEveManager::Create();
    TString event_string("event" + nEvent);
 
+   //Visualize clusters
    TEvePointSet* ps = new TEvePointSet();
-   //f->GetObject("SummaryPlots/setpointrandom", ps);
    f->GetObject(event_string+"/Ps_clusters", ps);
    ps->SetOwnIds(kTRUE);
    gEve->AddElement(ps);
    gEve->Redraw3D();
 
+   //Visualize hits in clusters
    TEvePointSet* ps2 = new TEvePointSet();
    f->GetObject(event_string+"/Ps_clHits", ps2);
    ps2->SetOwnIds(kTRUE);
    gEve->AddElement(ps2);
    gEve->Redraw3D();
 
+   //Visualize outliers
    TEvePointSet* ps3 = new TEvePointSet();
    f->GetObject(event_string+"/Ps_outliers", ps3);
    ps3->SetOwnIds(kTRUE);
